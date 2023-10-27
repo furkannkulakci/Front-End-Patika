50563057bc0fbd544f8044ada321b5937864ba97
Furkan Kulakci <furkankulakci@gmail.com>
2023-10-27 23:12:45 +0300

    added image

--- 04-JavaScript-category/01-homework/clock.js
+++ 04-JavaScript-category/01-homework/clock.js
@@ -1,5 +1,5 @@
 // kullanıcıdan adını girmesini istiyoruz.
-// let inputName = prompt("Please enter your name");
+let inputName = prompt("Please enter your name");
 let myName = document.querySelector("#myName");
 
 myName.innerHTML = `${myName.innerHTML} ${inputName}`;
