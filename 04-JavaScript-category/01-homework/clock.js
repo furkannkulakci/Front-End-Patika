76463ccc660e17996418d2f27e27e6bccbf368a3
Furkan Kulakci <furkankulakci@gmail.com>
2023-10-27 22:59:51 +0300

    first commit

---
+++ 04-JavaScript-category/01-homework/clock.js
@@ -0,0 +1,27 @@
+// kullanıcıdan adını girmesini istiyoruz.
+// let inputName = prompt("Please enter your name");
+let myName = document.querySelector("#myName");
+
+myName.innerHTML = `${myName.innerHTML} ${inputName}`;
+
+function showTime() {
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+
+  var days = [
+    "Pazar",
+    "Pazartesi",
+    "Sali",
+    "Carsamba",
+    "Persembe",
+    "Cuma",
+    "Cumartesi",
+  ];
+  dayName = days[d.getDay()];
+
+  document.querySelector("#myClock").innerHTML = `${h}:${m}:${s} ${dayName}`;
+}
+
+setInterval(showTime, 1000);
